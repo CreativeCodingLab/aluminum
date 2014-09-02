35effc27e0e42ff7efa340ec8121a46165b68b68
angusforbes <angus@delmak-o.local.tld>
2014-09-01 19:25:37 -0500

    Edits the run.sh in the basic demo to make it more clear how to edit the $INCLUDE_DIR variable to point to the GLM headers

--- osx/examples/basic/run.sh
+++ osx/examples/basic/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#default parameters of shell script will precompile headers, make a static lib, compile example, and then run the example. See aluminum/linux/run.command and the scripts in aluminum/linux/scripts/ for details
+#default parameters of shell script will precompile headers, make a static lib, compile example, and then run the example. See aluminum/osx/run.command and the scripts in aluminum/osx/scripts/ for details
 
-#you will probably have to edit aluminum/linux/run.command to point to the include files associated with GLM
+#you will probably have to edit aluminum/osx/run.command to point to the include files associated with GLM
 
 ../../run.command Basic.mm
